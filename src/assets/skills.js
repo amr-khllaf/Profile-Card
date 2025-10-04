9f1ec4e5f463def954e986ea7842a728e9dd3dd1
amr-khllaf <amr.a.khllaf@gmail.com>
2025-10-04 11:10:52 +0300

    Finish Profile Card

--- src/assets/skills.js
+++ src/assets/skills.js
@@ -1,30 +1,36 @@
-export const skills = [
+export const skillsList = [
   {
+    id: 1,
     skill: "HTML+CSS",
     level: "advanced",
     color: "#2662EA",
   },
   {
+    id: 2,
     skill: "JavaScript",
     level: "advanced",
     color: "#EFD81D",
   },
+  // {
+  //   id: 3,
+  //   skill: "Web Design",
+  //   level: "advanced",
+  //   color: "#C3DCAF",
+  // },
   {
-    skill: "Web Design",
-    level: "advanced",
-    color: "#C3DCAF",
-  },
-  {
+    id: 4,
     skill: "Git and GitHub",
     level: "intermediate",
     color: "#E84F33",
   },
   {
+    id: 5,
     skill: "React",
     level: "advanced",
     color: "#60DAFB",
   },
   {
+    id: 6,
     skill: "Svelte",
     level: "beginner",
     color: "#FF3B00",
